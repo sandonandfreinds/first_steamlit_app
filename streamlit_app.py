c2990c7b977d317570a73bdc0941c1ccf7697d19
sandonandfreinds <51063433+sandonandfreinds@users.noreply.github.com>
2022-07-18 16:07:50 +0100

    Update streamlit_app.py
    dumb filter

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,7 +17,7 @@ my_fruit_list = my_fruit_list.set_index('Fruit')
 streamlit.dataframe(my_fruit_list)
 
 #pick list
-streamlit.multiselect("Pick some fruits: ", list(my_fruit_list.index))
+streamlit.multiselect("Pick some fruits: ", list(my_fruit_list.index), ['Avocado', 'Strawberries'])
 
 #display list
 streamlit.dataframe(my_fruit_list)
