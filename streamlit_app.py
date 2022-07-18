7b8da6ac70b88145f06c95666aa549604afb968b
sandonandfreinds <51063433+sandonandfreinds@users.noreply.github.com>
2022-07-18 16:10:14 +0100

    Update streamlit_app.py
    smart filter

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,7 +17,8 @@ my_fruit_list = my_fruit_list.set_index('Fruit')
 streamlit.dataframe(my_fruit_list)
 
 #pick list
-streamlit.multiselect("Pick some fruits: ", list(my_fruit_list.index), ['Avocado', 'Strawberries'])
+fruits_selected = streamlit.multiselect("Pick some fruits: ", list(my_fruit_list.index), ['Avocado', 'Strawberries'])
+fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 #display list
-streamlit.dataframe(my_fruit_list)
+streamlit.dataframe(fruits_to_show)
