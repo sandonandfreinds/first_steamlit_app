964599f2fcbc54683e5c6b62369496efe93c399d
sandonandfreinds <51063433+sandonandfreinds@users.noreply.github.com>
2022-07-25 11:41:11 +0100

    Update streamlit_app.py
    parsed json data in data frame

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,3 +29,7 @@ streamlit.header('Fruityvice Fruit Advice!')
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response.json())
 
+# formats json data for UI
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# displays json data in panda dataframe 
+streamlit.dataframe(fruityvice_normalized)
