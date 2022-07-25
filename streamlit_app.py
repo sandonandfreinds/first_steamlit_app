0346abe85bec1dc1923b0a7202c8aea1649b126c
sandonandfreinds <51063433+sandonandfreinds@users.noreply.github.com>
2022-07-25 16:14:20 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -40,15 +40,6 @@ try:
 except URLError as e:
   streamlit.error()
 
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
-
-
-# formats json data for UI
-fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
-# displays json data in panda dataframe 
-
 
 streamlit.stop()
 ##############
