9364f65fd5425c685e9f306c977282f488ac6dd3
sandonandfreinds <51063433+sandonandfreinds@users.noreply.github.com>
2022-07-25 11:42:59 +0100

    Update streamlit_app.py
    added concentation to url string

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,8 +26,8 @@ streamlit.dataframe(fruits_to_show)
 
 streamlit.header('Fruityvice Fruit Advice!')
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+"kiwi")
+
 
 # formats json data for UI
 fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
